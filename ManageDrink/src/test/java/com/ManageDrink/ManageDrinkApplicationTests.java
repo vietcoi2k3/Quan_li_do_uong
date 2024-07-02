f0044292f4ab5ee1df67f897654c47cc7fc2111c
Quang Huy <huynobi1234@gmail.com>
2024-07-02 11:13:41 +0700

    khoi tao du an

---
+++ ManageDrink/src/test/java/com/ManageDrink/ManageDrinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ManageDrink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageDrinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
